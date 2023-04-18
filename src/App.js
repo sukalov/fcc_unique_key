3c7d7b0775b202f9f2e998ff9180662ced271dda
sukalov <matvey10@gmail.com>
2023-04-18 20:10:35 +0300

    r

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+const frontEndFrameworks = [
+  'React',
+  'Angular',
+  'Ember',
+  'Knockout',
+  'Backbone',
+  'Vue'
+];
 
 function App() {
+  const renderFrameworks = frontEndFrameworks.map(el => 
+  <li index={frontEndFrameworks.indexOf(el)} key={frontEndFrameworks.indexOf(el)}>{el}</li>
+); // Change this line
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Popular Front End JavaScript Frameworks</h1>
+      <ul>
+        {renderFrameworks}
+      </ul>
     </div>
   );
-}
+};
 
-export default App;
+export default App
